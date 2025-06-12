643a37dfc2c62b7faf7cd19e780ea4c478073336
Netanel <segal.netanel4@gmail.com>
2025-06-12 16:03:37 +0300

    added basic styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,21 @@ import { links } from '../App';
 
 export default function Navbar() {
   return (
-    <div>
-      <img className='logo' src='' alt='logo' />
-      <ul>
+    <nav className='flex flex-col md:flex-row md:justify-between bg-amber-400 px-6 py-3 items-center gap-3 md:gap-0'>
+      <h4 className='font-bold text-2xl'>Logo</h4>
+
+      <ul className='flex flex-row gap-2 items-center'>
         {links.map((link) => (
-          <li className='w-10 h-5 flex gap' key={link.path}>
-            <Link to={link.path}>{link.title}</Link>
+          <li key={link.path}>
+            <Link
+              to={link.path}
+              className='hover:text-white hover:underline transition duration-300'
+            >
+              {link.title}
+            </Link>
           </li>
         ))}
       </ul>
-    </div>
+    </nav>
   );
 }
