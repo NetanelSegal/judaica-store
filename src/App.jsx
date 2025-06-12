43c95598c2ee91c5e4a9c477a744f6488cf74bd7
Netanel <segal.netanel4@gmail.com>
2025-06-12 15:39:45 +0300

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import { Route, Routes } from 'react-router';
 import './App.css';
 import ProductsPage from './pages/ProductsPage';
 import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
+
+export const links = [
+  { path: '/', title: 'Home', element: <HomePage /> },
+  { path: '/products', title: 'Products', element: <ProductsPage /> },
+];
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path='/products' element={<ProductsPage />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        {links.map((link) => (
+          <Route path={link.path} element={link.element} />
+        ))}
+      </Routes>
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,17 @@
+import { Link } from 'react-router';
+import { links } from '../App';
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <img className='logo' src='' alt='logo' />
+      <ul>
+        {links.map((link) => (
+          <li className='w-10 h-5 flex gap' key={link.path}>
+            <Link to={link.path}>{link.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
