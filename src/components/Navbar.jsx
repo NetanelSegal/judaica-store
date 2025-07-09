a6812ac3891c43875db817712870cf48818062da
Netanel <segal.netanel4@gmail.com>
2025-07-09 10:59:06 +0300

    save

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
-import { Link } from 'react-router';
-import { links } from '../App';
+import { Link } from "react-router";
+import { links } from "../App";
 
 export default function Navbar() {
   return (
-    <nav className='flex flex-col md:flex-row md:justify-between text-white bg-[#22333B] px-6 py-3 items-center gap-3 md:gap-0 shadow-sm'>
-      <h4 className='font-bold text-2xl '>Logo</h4>
-      <ul className='flex flex-row gap-2 items-center'>
+    <nav className="flex flex-col md:flex-row md:justify-between text-white bg-[#22333B] px-6 py-3 items-center gap-3 md:gap-0 shadow-sm">
+      <h4 className="font-bold text-2xl ">Logo</h4>
+      <ul className="flex flex-row gap-2 items-center">
         {links.map(({ title, path }) => (
           <li key={path}>
             <Link
               to={path}
-              className='hover:text-white hover:underline transition duration-300'
+              className="hover:text-white hover:underline transition duration-300"
             >
               {title}
             </Link>
