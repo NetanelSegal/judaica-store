cbebab760e1f58f45a9c3c4eae1d1924dc30e204
Netanel <segal.netanel4@gmail.com>
2025-07-10 16:04:23 +0300

    changes navbar and app

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,53 @@ import ProductsPage from "./pages/ProductsPage/ProductsPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import AuthContext from "./contexts/AuthContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+export const loggedInLinks = [
+  {
+    path: "/logout",
+    title: "Logout",
+    element: (
+      <div>
+        <h1>Logout</h1>
+      </div>
+    ),
+  },
+];
+
+export const notLoggedInLinks = [
+  { path: "/login", title: "Login", element: <LoginPage /> },
+  {
+    path: "/signup",
+    title: "Signup",
+    element: <SignupPage />,
+  },
+];
 
 export const links = [
   { path: "/", title: "Home", element: <HomePage /> },
   { path: "/products", title: "Products", element: <ProductsPage /> },
-  { path: "/login", title: "Login", element: <LoginPage /> },
-  { path: "/signup", title: "Signup", element: <SignupPage /> },
 ];
 
 function App() {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/auth/validate", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    validateToken();
+  }, []);
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <Navbar />
@@ -24,6 +60,14 @@ function App() {
         {links.map(({ path, element }) => (
           <Route path={path} element={element} />
         ))}
+
+        {user
+          ? loggedInLinks.map(({ path, element }) => (
+              <Route path={path} element={element} />
+            ))
+          : notLoggedInLinks.map(({ path, element }) => (
+              <Route path={path} element={element} />
+            ))}
       </Routes>
     </AuthContext.Provider>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,29 @@
 import { useContext } from "react";
 import { Link } from "react-router";
-import { links } from "../App";
+import { links, loggedInLinks, notLoggedInLinks } from "../App";
 import AuthContext from "../contexts/AuthContext";
 
 export default function Navbar() {
   const { user } = useContext(AuthContext);
-  console.log(user);
+  const renderLink = ({ title, path }) => (
+    <li key={path}>
+      <Link
+        to={path}
+        className="hover:text-white hover:underline transition duration-300"
+      >
+        {title}
+      </Link>
+    </li>
+  );
 
   return (
     <nav className="flex flex-col md:flex-row md:justify-between text-white bg-[#22333B] px-6 py-3 items-center gap-3 md:gap-0 shadow-sm">
       <h4 className="font-bold text-2xl ">Logo</h4>
       <ul className="flex flex-row gap-2 items-center">
-        {links.map(({ title, path }) => (
-          <li key={path}>
-            <Link
-              to={path}
-              className="hover:text-white hover:underline transition duration-300"
-            >
-              {title}
-            </Link>
-          </li>
-        ))}
+        {links.map(renderLink)}
+        {user
+          ? loggedInLinks.map(renderLink)
+          : notLoggedInLinks.map(renderLink)}
       </ul>
     </nav>
   );
